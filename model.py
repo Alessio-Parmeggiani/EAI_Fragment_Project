365efc7d37f80ce18ba083aba4a1f3c3acafcca3
parmex <parmeggiani.1799327@studenti.uniroma1.it>
2022-01-25 11:27:12 +0100

    sono sbaglaiti gli args

--- model.py
+++
@@ -1,8 +0,0 @@
-import open3d as o3d
-
-mesh = o3d.io.read_triangle_mesh("file.stl")
-pointcloud = mesh.sample_points_poisson_disk(100000)
-
-# you can plot and check
-o3d.visualization.draw_geometries([mesh])
-o3d.visualization.draw_geometries([pointcloud])
